3c1abda1beb5baa6ad1a8f8fd697ae72357764de
Tareq-Ahmed97 <107646016+Tareq-Ahmed97@users.noreply.github.com>
2022-08-15 14:21:57 +0600

    authentication

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30 mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30 mb", extended: true }));
+app.use(cors());
+app.use("/posts", postRoutes);
+app.use("/", (req, res) => res.send("Home Page"));
+
+const CONNECTION_URL =
+  "mongodb+srv://tareq9725:peterparker9725@socialapp.4oyc69f.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  .then(() =>
+    app.listen(PORT, () => console.log("Server running on port: ${PORT}"))
+  )
+  .catch((error) => console.log(error.message));
